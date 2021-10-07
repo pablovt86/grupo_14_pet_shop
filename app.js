8a7de334b419de05471b943edbb19aa9c1457b36
Ulises-Armolla <uliarmolla@gmail.com>
2021-10-07 19:55:59 -0300

    Servidor funcionando!

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('espress');
+const express = require('express');
 const app = express();
 const path = require('path');
 let port = 3000;
 
-app.use(static("public"))
+app.use(express.static("public"));
 
-app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`))
+app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`));
