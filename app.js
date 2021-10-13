901f5d4d5b197a639828f1b19d1a77f37757327f
pablovt86 <pablo.vt@hotmail.com.ar>
2021-10-13 11:40:06 -0300

    se arma la diferente hojas de styles

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('espress');
+const express = require('express');
 const app = express();
 const path = require('path');
 let port = 3000;
 
-app.use(static("public"))
+ app.use(express.static("public"))
 
+app.get('/productDetail', function(req, res){
+    res.sendFile(path.join(__dirname, 'views/productDetail.htm'))
+
+})
 app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`))
