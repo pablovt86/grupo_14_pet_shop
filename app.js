6b687a5e2793abf50a322533c299efa70dc63a90
pablovt86 <pablo.vt@hotmail.com.ar>
2021-10-22 16:00:31 -0300

    se acomoda los stylos y la vista para el carrito

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const app = express();
 const path = require('path');
 let port = 3000;
 
-<<<<<<< HEAD
  app.use(express.static("public"))
 
 app.get('/productDetail', function(req, res){
     res.sendFile(path.join(__dirname, 'views/productDetail.htm'))
 
 })
-app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`))
-=======
-app.use(express.static("public"));
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'views/index.html'))
 
-app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`));
->>>>>>> 8a7de334b419de05471b943edbb19aa9c1457b36
+})
+app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`))
+
