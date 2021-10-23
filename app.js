a51861419ce285b86d79831bb9e56edc9793bd41
Ulises-Armolla <uliarmolla@gmail.com>
2021-10-22 21:27:05 -0300

    Subo html de login, register, forms.css e imagenes correspondientes

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 const path = require('path');
 let port = 3000;
 
- app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get('/productDetail', function(req, res){
-    res.sendFile(path.join(__dirname, 'views/productDetail.htm'))
+    res.sendFile(path.join(__dirname, 'views/productDetail.htm'));
 
-})
+});
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'views/index.html'))
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-})
-app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`))
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register', function(req, res){
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+app.listen(port, ()=>console.log(`servidor Escuchando en Puerto ${port} http://localhost:${port}`));
 
