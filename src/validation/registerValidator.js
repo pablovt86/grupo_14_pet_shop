b7944d31059333f224607f86feb27d8e6c1b8381
Ulises Armolla <uliarmolla@gmail.com>
2022-01-22 23:05:49 -0500

    subo vistas login y register con su css, refactorizado

--- src/validation/registerValidator.js
+++ src/validation/registerValidator.js
@@ -5,22 +5,22 @@ const { users } = require('../data/dataBase')
 module.exports = [
     check('name')
     .notEmpty()
-    .withMessage('El nombre es requerido'),
+    .withMessage('El nombre es requerido.'),
 
     check('last_name')
     .notEmpty()
-    .withMessage('El apellido es requerido'),
+    .withMessage('El apellido es requerido.'),
     check('tel')
     .notEmpty()
-    .withMessage('el campo no puede estar vacio').isLength({
+    .withMessage('el campo no puede estar vacio.').isLength({
         min: 10,
         max: 10
     })
-    .withMessage('el campo debe tener 10 caracteres'),
+    .withMessage('el campo debe tener 10 caracteres.'),
 
     check('email')
     .isEmail()
-    .withMessage('Debes ingresar un email válido'),
+    .withMessage('Debes ingresar un email válido.'),
 
     body('email').custom((value, {req}) => {
       
@@ -37,23 +37,23 @@ module.exports = [
             return true
         }
    
-    }).withMessage('Email ya registrado'),
+    }).withMessage('Email ya registrado.'),
 
     check('pass1')
     .notEmpty()
-    .withMessage('Debes escribir tu contraseña')
+    .withMessage('Debes escribir tu contraseña.')
     .isLength({
         min: 6,
         max: 12
     })
-    .withMessage('La contraseña debe tener entre 6 y 12 caracteres'),
+    .withMessage('La contraseña debe tener entre 6 y 12 caracteres.'),
 
     body('pass2').custom((value, {req}) => value !== req.body.pass1 ? false : true)
-    .withMessage('Las contraseñas no coinciden'),
+    .withMessage('Las contraseñas no coinciden.'),
 
     check('terms')
     .isString('on')
-    .withMessage('Debes aceptar las bases y condiciones')
+    .withMessage('Debes aceptar los términos.')
 
 
 ]
