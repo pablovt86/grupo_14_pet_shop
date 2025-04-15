1417e231bf5c1b26a550f6c5c73ae2c1ceb00580
Pablo Vaccotti <53192164+pablovt86@users.noreply.github.com>
2025-04-15 12:29:04 -0300

    Update config.js
    modificacion con variables de entornos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "dbnegociopetshop",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
   }
-}
+};
