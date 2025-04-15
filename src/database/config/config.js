6e165759b572ee59ea9611c6c515dd91aa6dd049
Pablo Vaccotti <53192164+pablovt86@users.noreply.github.com>
2025-04-15 16:40:36 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,20 +6,24 @@ module.exports = {
     password: process.env.DB_PASSWORD === 'null' ? null : process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   test: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_PROD,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   }
 };
+
