58888ce550a26a34a97dde6c83e9efe924b2fc65
Pablo Vaccotti <53192164+pablovt86@users.noreply.github.com>
2025-04-16 15:02:23 -0300

    Update homeController.js

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,27 @@
-let controller = {
-    index: (req, res) => {
-     
-        let titulo = "Bienvenidos a Fatiga Pet Shop";
-        res.render('home', {titulo,
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require('sequelize');
+const newHomeController = {
+    home:(req, res)=>{
+        let images = db.ProductImage.findAll()
+
+        let productsInSale = db.Product.findAll({
+            where:{
+                discount:{[Op.gte] : 15}
+            }
+        })
+        Promise.all([productsInSale,images])
+        .then(([productsInSale,images]) => { 
+        res.render('home', {
+            sliderTitle: "PROMOCIONES",
+            sliderProducts: productsInSale,
+            title:"home",
+            images,
             session: req.session
-        
-        });
-    }
+        })
+    })
+    
+}
 }
 
-module.exports = controller;
+module.exports = newHomeController;
