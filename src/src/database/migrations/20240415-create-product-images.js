600b391c1820f52857756ec401a4d471be97a2dd
Pablo Vaccotti <53192164+pablovt86@users.noreply.github.com>
2025-04-16 11:17:18 -0300

    Create 20240415-create-product-images.js

---
+++ src/src/database/migrations/20240415-create-product-images.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('product_images', {
+      idproduct_images: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      images: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      idproducts: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'products',
+          key: 'idproducts'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('product_images');
+  }
+};
