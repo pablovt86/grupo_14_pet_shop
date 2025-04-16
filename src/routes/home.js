4885d1c2986ca8e0330a17c9072ea46b2e587cb3
Pablo Vaccotti <53192164+pablovt86@users.noreply.github.com>
2025-04-16 15:03:50 -0300

    Update home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,6 +2,6 @@ let express = require('express');
 let router = express.Router();
 let homeController = require('../controller/homeController');
 
-router.get('/', homeController.index);
+router.get('/', homeController.home);
 
 module.exports = router;
